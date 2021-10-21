927fe25ce27ee0164549d52d64f6071a03b0b809
Cozac98 <ale.cozacov@gmail.com>
2021-10-21 16:07:48 -0300

    WIP

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Navbar from "./components/NavBar/Navbar";
+import Landing from "./components/Landing/Landing";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Landing />
     </div>
   );
 }
