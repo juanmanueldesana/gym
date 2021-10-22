ae68deecc69d6d7a881a004f33a0c799b1e80589
Cozac98 <ale.cozacov@gmail.com>
2021-10-22 10:51:05 -0300

    logo+routeo

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import React from 'react';
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
 import Navbar from "./components/NavBar/Navbar";
 import Landing from "./components/Landing/Landing";
 import './App.css';
 
-function App() {
+export default function App() {
   return (
+    <Router>
     <div className="App">
       <Navbar />
-      <Landing />
+      <Switch>
+        <Route path="/">
+          <Landing />
+        </Route>
+      </Switch>
     </div>
+    </Router>
   );
 }
 
-export default App;
