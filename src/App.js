39a2fd85e8dce1916e6b343adf7b54a0e6d9bd0b
Juan Manuel <juanmanuel.manuel70@gmail.com>
2021-11-10 12:28:07 -0300

    Agregado perfil al front, boton guardar cambios queda pendiente

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Landing from "./components/Landing/Landing";
 import Login from "./components/Login/Login"
 import Clases from "./components/Clases/Clases"
 import Staff from "./components/Staff/Staff"
+import Perfil from "./components/Perfil/Perfil"
 import Separador from "./components/Separador/Separador";
 import Footer from "./components/Footer/Footer"
 import WapButton from "./components/WapButton/WapButton"
@@ -24,6 +25,9 @@ export default function App() {
         </Route>
         <Route path="/Staff">
           <Staff />
+        </Route>
+        <Route path="/Perfil">
+          <Perfil />
         </Route>
         <Route path="/">
           <Landing />
