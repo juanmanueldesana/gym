dfbcab5db1676b75fbeb12559ffa7f5f44303667
gustavodejon <89876391+gustavodejon@users.noreply.github.com>
2021-11-15 21:08:53 -0300

    no message

--- src/components/Staff/Staff.js
+++ src/components/Staff/Staff.js
@@ -1,53 +1,51 @@
 
 
-import './Clases.css';
-import zumbaimg from "./Images/zumba.jpg"
-import bodycombatimg from "./Images/bodycombat.jpg"
-import taekwondoimg from "./Images/taekwondo.jpg"
-import bodypumpimg from "./Images/bodypump.jpg"
-import powerjumpimg from "./Images/powerjump.jpg"
-import crossfitimg from "./Images/crossfit.jpg"
-import funcionalimg from "./Images/funcional.jpg"
-import taeboimg from "./Images/taebo.jpg"
-import bicicletaimg from "./Images/bicicleta.jpg"
-import pilatesimg from "./Images/pilates.jpg"
-import yogaimg from "./Images/yoga.jpg"
-import musculacionimg from "./Images/musculacion.jpg"
+import './Staff.css';
+import juancarlosimg from "./Images/juancarlos.jpg"
+import agustinaimg from "./Images/agustina.jpg"
+import florenciaeignacioimg from "./Images/florenciaeignacio.jpg"
+import guadalupeimg from "./Images/guadalupe.jpg"
+import ronaldoimg from "./Images/ronaldo.jpg"
+import eugenioimg from "./Images/eugenio.jpg"
+import robertojpg from "./Images/roberto.jpg"
+import jenifferimg from "./Images/jeniffer.jpg"
+import pamelaimg from "./Images/pamela.jpg"
+import lucianaimg from "./Images/luciana.jpg"
+import karinaimg from "./Images/karina.jpg"
+import carolinaimg from "./Images/carolina.jpg"
 
 export default function Staff(props) {
-    const clases = [
-        {name: "Zumba", horario: "Lunes- Miércoles y Viernes: 10:00 hs.", foto: zumbaimg},
-        {name: "Body-Combat", horario: "Lunes- Miércoles y Viernes: 12:00 hs.", foto: bodycombatimg},
-        {name: "Tae-Kwon-Do", horario: "Lunes- Miércoles y Viernes: 14:00 hs.", foto: taekwondoimg},
-        {name: "BodyPump", horario: "Lunes- Miércoles y Viernes: 16:00 hs.", foto: bodypumpimg},
-        {name: "Power-Jump", horario: "Lunes- Miércoles y Viernes: 18:00 hs.", foto: powerjumpimg},
-        {name: "CrossFit", horario: "Lunes- Miércoles y Viernes: 20:00 hs.", foto: crossfitimg},
-        {name: "Funcional", horario: "Martes y Jueves: 10:00 hs.", foto: funcionalimg},
-        {name: "TaeBo", horario: "Martes y Jueves: 12:00 hs.", foto: taeboimg},
-        {name: "Bicicleta", horario: "Martes y Jueves: 14:00 hs.", foto: bicicletaimg},
-        {name: "Pilates", horario: "Martes y Jueves: 16:00 hs.", foto: pilatesimg},
-        {name: "Yoga", horario: "Martes y Jueves: 18:00 hs.", foto: yogaimg},
-        {name: "Musculación", horario: "Todos los días de 10:00 a 22:00 hs.", foto: musculacionimg}
+    const staff = [
+        {name: "Juan Carlos", profesor: "PROFESOR DE: ZUMBA!", foto: juancarlosimg},
+        {name: "Agustina", profesor: "PROFESORA DE: BODY COMBAT!", foto: agustinaimg},
+        {name: "Flor y Nacho", profesor: "PROFESORES DE: TAE-KWON-DO!", foto: florenciaeignacioimg},
+        {name: "Guadalupe", profesor: "PROFESORA DE: BODY PUMP!", foto: guadalupeimg},
+        {name: "Ronaldo", profesor: "PROFESOR DE: POWER JUMP!", foto: ronaldoimg},
+        {name: "Eugenio", profesor: "PROFESOR DE: CROSSFIT!", foto: eugenioimg},
+        {name: "Roberto", profesor: "PROFESOR DE: FUNCIONAL!", foto: robertojpg},
+        {name: "Jeniffer", profesor: "PROFESORA DE: TAE-BO!", foto: jenifferimg},
+        {name: "Pamela", profesor: "PROFESORA DE: SPINNING!", foto: pamelaimg},
+        {name: "Luciana", profesor: "PROFESORA DE: PILATES!", foto: lucianaimg},
+        {name: "Karina", profesor: "PROFESORA DE: YOGA!", foto: karinaimg},
+        {name: "Carolina", profesor: "PROFESORA DE: MUSCULACIÓN!", foto: carolinaimg}
     ]
     return (
         <div>
             <div className="main-div">
-                <h1 className="custon-title">CLASES PERSONALIZADAS</h1>
-                <p className="description"> En Gimnasio Musculito contamos con clases adaptadas a tus necesidades, y rutinas
-                    hechas a tu medida!</p>
+                <h1 className="custon-title">LOS MEJORES PROFESORES!</h1>
+                <p className="description"> En Gimnasio Musculito contamos con el mejor staff de profesores, siempre capacitados con las últimas tendencias!</p>
             </div>
             <div className="all-cards">
                 {
-                    clases.map((clase)=> {
+                    staff.map((staff)=> {
                         return (
                             <div className={"card-container-custom"}>
                                 <div className="card">
-                                    <img src={clase.foto} className="card-img-top" alt="..."/>
+                                    <img src={staff.foto} className="card-img-top" alt="..."/>
                                     <div className="card-body">
-                                        <h5 className="card-title">{clase.name}</h5>
-                                        <p className="card-text">{clase.name} es la disciplina del presente. Diversión, salud y estar en forma,
-                                            TODO combinado con la mejor de las ondas.</p>
-                                        <a href="#" className="btn btn-primary">{clase.horario}</a>
+                                        <h5 className="card-title">{staff.name}</h5>
+                                        <p className="card-text">{staff.name} pertenece a nuestro Staff profesionalizado. Te animás a tomar su clase??</p>
+                                        <a href="#" className="btn btn-primary">{staff.profesor}</a>
                                     </div>
                                 </div>
                             </div>
